c644f6458cbd7899bb1c552d864d09930f427f77
Aurelien Ginolhac <ginolhac@gmail.com>
2016-10-18 14:04:37 +0200

    add missing docs for datasets

---
+++ R/garden.R
@@ -0,0 +1,12 @@
+#' ozone concentration in 3 different gardens.
+#'
+#' A (fake) dataset from Michael Crawley
+#'
+#'
+#' @format A tibble with 10 rows and 3 variables:
+#' \describe{
+#'   \item{gardenA}{ozone concentration for gardenA}
+#'   \item{gardenB}{ozone concentration for gardenB}
+#'   \item{gardenC}{ozone concentration for gardenC}
+#' }
+"garden"

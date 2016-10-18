f0b8d846bd26a574898dc3c40badb08b3cfd0d1e
Aurelien Ginolhac <ginolhac@gmail.com>
2016-10-18 13:55:38 +0200

    add wings dataset

---
+++ R/wings.R
@@ -0,0 +1,11 @@
+#' Size of insect wings on 2 different locations.
+#'
+#' A (fake) dataset from Michael Crawley
+#'
+#'
+#' @format A tibble with 120 rows and 2 variables:
+#' \describe{
+#'   \item{size}{size, in mm}
+#'   \item{location}{A or B}
+#' }
+"wings"
